952bd389ec2ff27343f1c35768b67fc47a28ebd3
kitakou0313 <kitakou0313@gmail.com>
2020-12-05 02:32:16 +0900

    Add impl of problems

--- repository/problem_impl.go
+++ repository/problem_impl.go
@@ -4,3 +4,14 @@ func (repo *GormRepository) GetProblem(id string) (*Problem, error) {
 	// not implemented yet
 	return nil, nil
 }
+
+func (repo *GormRepository) GetListOfProblem() ([]*Problem, error) {
+	// not implemented yet
+	return nil, nil
+}
+
+func (repo *GormRepository) JudgeFlag(id string) (bool, error) {
+	// not implemented yet
+	return false, nil
+}
+
