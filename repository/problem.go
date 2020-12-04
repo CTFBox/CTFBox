16d28cdda7f073036d10e250d9bd54fcc2d35dc6
kitakou0313 <kitakou0313@gmail.com>
2020-12-05 02:02:13 +0900

    Add interface for problem

--- repository/problem.go
+++ repository/problem.go
@@ -5,8 +5,14 @@ import "github.com/gofrs/uuid"
 type Problem struct {
 	ID uuid.UUID `db:"id"`
 	// add labels
+	title string 
+	score: int,
+	solved: int
+	star: int
 }
 
 type ProblemRepository interface {
 	GetProblem(id string) (*Problem, error)
+	GetListOfProblem() ([]*Problem, error)
+	JudgeFlag(id string) (bool, error)
 }
